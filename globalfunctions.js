603cfc99c309ec980ec0561f3c1f3084d8111b2d
Gabe <gabriel.philbois@gmail.com>
2022-05-28 17:26:36 -0400

    export function almost ready

--- globalfunctions.js
+++ globalfunctions.js
@@ -1,17 +1,39 @@
 
-
-function printObjects(arr2){
+//arr2 is the array of objects that will be printed
+//printSheet is the sheet where it wil be printed
+function printObjects(arr2,printSheet){
     arr1 = Object.keys(arr2[0]);
 
     arr3=[];
     arr3[0] = arr1;
 
-    let i = 0;
+//sort arr2 by the ID key, can't sort arr3 directly, because it would mess up the title row
 
+    arr2.sort((a, b) => {
+        return a.ID - b.ID;
+    });
+
+//fill oput the array that will be printed
+    let i = 0;
     while(i < arr2.length){
         arr3[i+1] = arr2[i];
     }
-    
+
+
+//clear sheet
+    //e.g.
+    //InvoicesSheet.getRange(1,1,1000,30).clearContent();
+    printSheet.getRange(1,1,1000,30).clearContent();
+
+
+
+//print array
+    //e.g
+    // InvoicesSheet.getRange(2,1,printArray.length,printArray[1].length).setValues(printArray);
+
+
+
+
 
 
 
